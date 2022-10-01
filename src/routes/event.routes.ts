743b904b4a9127061623de36661f91b59ed7a4e1
Diego Cahuayo <diegocahuayo@Diegos-MacBook-Pro.local>
2022-10-01 10:42:04 -0500

    feat: Update event route

--- src/routes/event.routes.ts
+++ src/routes/event.routes.ts
@@ -1,7 +1,14 @@
 import express, { Router } from 'express'
 import asyncHandler from 'express-async-handler'
 import passport from 'passport'
-import { createEvent } from '../controllers/event.controller'
+import {
+  allEvents,
+  createEvent,
+  deleteEvent,
+  eventsByUser,
+  updateEvent,
+  eventById,
+} from '../controllers/event.controller'
 import { validateUser } from '../guards/user.guard'
 
 const router = express.Router()
@@ -10,7 +17,20 @@ export function eventRoutes(): Router {
   router
     .route('/')
     .all(passport.authenticate('jwt', { session: false }), validateUser)
+    .get(asyncHandler(allEvents))
     .post(asyncHandler(createEvent))
+
+  router
+    .route('/user')
+    .all(passport.authenticate('jwt', { session: false }), validateUser)
+    .get(asyncHandler(eventsByUser))
+
+  router
+    .route('/:id')
+    .all(passport.authenticate('jwt', { session: false }), validateUser)
+    .get(asyncHandler(eventById))
+    .put(asyncHandler(updateEvent))
+    .delete(asyncHandler(deleteEvent))
 
   return router
 }
