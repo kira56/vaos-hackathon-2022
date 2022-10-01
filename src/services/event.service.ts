192adc5a4aecf9d2e75b2008a671e68bdb4c62fb
Diego Cahuayo <diegocahuayo@Diegos-MacBook-Pro.local>
2022-10-01 10:40:36 -0500

    feat: Implement metho getById on event service

--- src/services/event.service.ts
+++ src/services/event.service.ts
@@ -1,12 +1,13 @@
-import { User } from '@prisma/client'
 import { plainToClass, plainToInstance } from 'class-transformer'
 import { EventDao } from '../daos/event.dao'
 import { CreateEventDto } from '../dtos/request/event/create-event.dto'
+import { UpdateEventDto } from '../dtos/request/event/update-event.dto'
 import { EventDto } from '../dtos/response/event/event.dto'
-import { EventUserService } from './event-user.service'
+import { RegisterService } from './register.service'
 
 export class EventService {
   private static eventDao = new EventDao()
+  private static registerService = RegisterService
 
   static async all(): Promise<EventDto[]> {
     const events = await this.eventDao.all({})
@@ -14,10 +15,49 @@ export class EventService {
     return plainToInstance(EventDto, events)
   }
 
-  static async create(input: CreateEventDto): Promise<EventDto> {
-    const event = await this.eventDao.save(input)
-    // eslint-disable-next-line no-console
-    // await EventUserService.create(user.id, event.id)
+  static async getById(id: string): Promise<EventDto> {
+    const event = await this.eventDao.find({ id })
+
+    return plainToClass(EventDto, event)
+  }
+
+  static async eventCreateByUser(userId: string): Promise<EventDto[]> {
+    const events = await this.eventDao.all({
+      where: {
+        userId,
+      },
+    })
+
+    return plainToInstance(EventDto, events)
+  }
+
+  static async create(input: CreateEventDto, userId: string): Promise<EventDto> {
+    const event = await this.eventDao.save({
+      user: {
+        connect: {
+          id: userId,
+        },
+      },
+      ...input,
+    })
+
+    return plainToClass(EventDto, event)
+  }
+
+  static async update(id: string, input: UpdateEventDto): Promise<EventDto> {
+    const event = await this.eventDao.update(input, { id })
+
+    return plainToClass(EventDto, event)
+  }
+
+  static async delete(id: string) {
+    const registers = await this.registerService.registerByEvent(id)
+
+    if (registers) {
+      await Promise.all(registers.map((item) => this.registerService.hardDelete(item.id)))
+    }
+
+    const event = await this.eventDao.delete({ id })
 
     return plainToClass(EventDto, event)
   }
